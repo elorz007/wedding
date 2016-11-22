57680b82cd2cd7efa441a865678909af689fa5b0
Mikel Elorz <mikel.elorz@virtual-solution.com>
2016-11-22 18:16:20 +0100

    prepare

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,41 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      options: {
-        livereload: true,
-      },
-      html: {
-        files: ['*.html'],
+      css: {
+        files: '**/*.md',
+        tasks: ['includereplace'],
+        options: {
+          livereload: true,
+        },
       },
     },
+
+    // Use 'grunt includereplace' for generating preview (which is gitignored)
+    // Use 'grunt includereplace:dist' for generating the real distributing html
+    includereplace: {
+      preview: {
+        options: {
+          // Task-specific options go here.
+        },
+        // Files to perform replacements and includes with
+        src: 'slides.html',
+        // Destination directory to copy files to
+        dest: 'preview/'
+      },
+
+      dist: {
+        options: {
+          // Task-specific options go here.
+        },
+        // Files to perform replacements and includes with
+        src: 'slides.html',
+        // Destination directory to copy files to
+        dest: 'dist/'
+      }
+    }
+
   });
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-include-replace');
+
 };
